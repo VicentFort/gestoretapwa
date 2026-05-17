fbd7224e8919d19ffa4f29e766c2cd97dd0aa658
vfortro <vfortro@upv.edu.es>
2026-05-17 17:24:19 +0200

    Cambios de la visualización del panel de control (Pasa de adminAccess a AccessType)

--- src/stores/api.js
+++ src/stores/api.js
@@ -3,7 +3,7 @@ import { useLoadingStore } from "./loadingStore";
 
 
 const API_IP = "https://gestoretaapp-production.up.railway.app";
-const LOCAL_API_IP = "http://192.168.1.29:8080";
+const LOCAL_API_IP = "http://37.135.29.68:8080";
 const GOOGLE_CALLENDAR_API = 'https://www.googleapis.com/auth/calendar';
 const api = axios.create({
   baseURL: LOCAL_API_IP
